ad4effb6bd08bd8a9b5e6463c318d331e0e1f14b
zimoluo <zimo_luo@outlook.com>
2024-06-04 16:50:34 +0800

    rainbow~~~

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import arrangeFaviconGrid from "./src/arrangeFaviconGrid";
 import generateFavicon from "./src/generateFavicon";
 
-const themesList: string[] = ["glitter", "mori", "cubistic", "marina"];
+const themesList: string[] = [
+  "glitter",
+  "mori",
+  "cubistic",
+  "marina",
+  "springField",
+  "rainbow",
+];
 
 async function generateFavicons(themes: string[]) {
   for (const theme of themes) {
